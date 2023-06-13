a56ba42790a1bca7870d14b9c8a95db340b99f5e
E-Kat101 <74022900+E-Kat101@users.noreply.github.com>
2023-06-12 21:17:24 -0400

    Update keep_guessing.py

--- 4.3 Input in a Loop/keep_guessing.py
+++ 4.3 Input in a Loop/keep_guessing.py
@@ -1,10 +1,11 @@
 number = 4
 
 print("I have chosen a number between 1 and 10. Try to guess it.")
-guess = int(input("Your guess: "))
 
-while guess != number:
-    print("That is incorrect. Guess again.")
+while True:
     guess = int(input("Your guess: "))
+    if guess == 4:
+        print(f"That's right! The number was {number}. You're a good guesser.")
+        break
 
-print(f"That's right! The number was {number}. You're a good guesser.")
+    print("That is incorrect. Guess again.")
